d10b5cba05d1314ba6e1f10fded7ad9a19645178
paavo <shibaev.pp@phystech.edu>
2021-04-06 02:43:29 +0300

    fix minor changes

--- 1/server.lua
+++ 1/server.lua
@@ -1,21 +1,31 @@
-yaml = require('yaml')
-configFile = io.open("./config.yml", "r")
+local yaml = require('yaml')
+local fio = require('fio')
+local err
+configFile, err = fio.open("./config.yml", {'O_RDONLY'})
 if not configFile then
-    print("Error reading file")
-    return nil
+    print('Error reading file')
+    print(err)
+    os.exit(1)
 end
-content = configFile:read("all")
+
+local content = configFile:read()
 configFile:close()
-decoded = yaml.decode(content)
+err = nil
+decoded, err = yaml.decode(content)
+if err ~= nil then
+    print('Parsing error')
+    print(err)
+    os.exit(1)
+end
 
 -- parsing paramenters
 local inPort = tonumber(decoded['proxy']['port'])
 local host = decoded['proxy']['bypass']['host']
 local goalPort = tonumber(decoded['proxy']['bypass']['port'])
 
-http_client = require('http.client').new({})
+local http_client = require('http.client').new({})
 local function redirect(req)
-    url = host .. ":" .. goalPort .. req:path() .. "?" .. req:query()
+    local url = host .. ":" .. goalPort .. req:path() .. "?" .. req:query()
     return http_client:request(req:method(), url, req.body, {
         req:headers()
     })
